aadd84fba67588acee41197e6f366db5e7237add
dxng1410 <dung.dang1410@hcmut.edu.vn>
2025-06-13 14:01:30 +0700

    Delete Yolo_Uno directory

--- Yolo_Uno/src/device/TaskDHT20.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_TASKDHT20_H_
-#define INC_TASKDHT20_H_
-
-#include "globals.h"
-
-extern void initDHT20();
-extern void getValueDHT20();
-
-#endif /* INC_TASKDHT20_H_ */
--- Yolo_Uno/src/device/TaskPIRsensor.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_TASKPIR_H_
-#define INC_TASKPIR_H_
-
-#include "globals.h"
-
-extern void initPIR();
-extern void getValuePIR();
-
-#endif /* INC_TASKPIR_H_ */
